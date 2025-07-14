334a39e852ca7739b882eec89a60d367fd3f9e56
Jatin Vidhani <131987761+jatin-vidhani17@users.noreply.github.com>
2025-07-14 15:30:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { Server } = require('socket.io');
 const http = require('http');
 
 const app = express();
@@ -17,31 +18,32 @@ app.use(bodyParser.json());
 const emailToSocketMapping = new Map();
 const socketToEmailMapping = new Map();
 
-io.on("connection", (socket) => {
-    console.log("New Connection Established", socket.id);
-    socket.on("join-room", (data) => {
-        const {roomId, emailId} = data;
-        console.log('User',emailId,"Joined Room",roomId);
-        emailToSocketMapping.set(emailId,socket.id);
+io.on('connection', (socket) => {
+    console.log('New Connection Established', socket.id);
+    socket.on('join-room', (data) => {
+        const { roomId, emailId } = data;
+        console.log('User', emailId, 'Joined Room', roomId);
+        emailToSocketMapping.set(emailId, socket.id);
         socketToEmailMapping.set(socket.id, emailId);
         socket.join(roomId);
-        socket.emit('joined-room', { roomId});
-        socket.broadcast.to(roomId).emit("user-joined",{ emailId })
+        socket.emit('joined-room', { roomId });
+        socket.broadcast.to(roomId).emit('user-joined', { emailId });
     });
 
-    socket.on('call-user', data =>  data => {
-        const {emailId, offer} = data;
-        const fromEmail = socketToEmailMapping.get(socket.id); 
+    socket.on('call-user', (data) => {
+        const { emailId, offer } = data;
+        const fromEmail = socketToEmailMapping.get(socket.id);
         const socketId = emailToSocketMapping.get(emailId);
-        socket.to(socketId).emit('incoming-call', {from:fromEmail, offer})
+        socket.to(socketId).emit('incoming-call', { from: fromEmail, offer });
     });
 
-    socket.on('call-accepted',data =>{
-        const{emailId,ans} = data;
+    socket.on('call-accepted', (data) => {
+        const { emailId, ans } = data;
         const socketId = emailToSocketMapping.get(emailId);
-        socket.to(socketId).emit("call-accepted",{ans});
+        socket.to(socketId).emit('call-accepted', { ans });
     });
 });
 
+// Use the PORT environment variable provided by Render
 const PORT = process.env.PORT || 8000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
