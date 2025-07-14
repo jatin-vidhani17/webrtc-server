e81708946d23533f49d325bc366be0dfd5984372
Jatin Vidhani <131987761+jatin-vidhani17@users.noreply.github.com>
2025-07-14 15:23:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -38,5 +38,5 @@ io.on("connection", (socket) => {
     });
 });
 
-app.listen(8000,() => console.log("Http server running at PORT 8000"));
-io.listen(8001);
+const PORT = process.env.PORT || 8000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
